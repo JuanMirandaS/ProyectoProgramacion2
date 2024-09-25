cd11f67c892198af078d3e88f14bed7ea0e333f1
AhuEst <fabianestahuca@gmail.com>
2024-09-24 23:03:24 -0300

    Modelo Rol + Modelo Usuario

---
+++ ProyectoProgramacion2/Modelo/Rol.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoProgramacion2.Modelo
+{
+    public class Rol
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ ProyectoProgramacion2/Modelo/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoProgramacion2.Modelo
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RolId { get; set; }
+
+    }
+}
