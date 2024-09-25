2a52871b01dbca95e37d0e3f6af5fa509a89998e
juan miranda <jpmiranda8183@gmail.com>
2024-09-24 21:31:36 -0300

    Modelo Proyecto

--- ProyectoProgramacion2/Modelo/Proyecto.cs
+++ ProyectoProgramacion2/Modelo/Proyecto.cs
@@ -2,6 +2,12 @@
 {
     public class Proyecto
     {
-        
+        public int Id { get; set; }
+        public required string Nombre  { get; set; }
+        public required string Descripcion { get; set; }
+        public required string Estado { get; set; }
+        public int HorasTrabajadas { get; set; }
+        public required int HorasTotales { get; set; }
+        public required DateTime FechaCreacion { get; set; }
     }
 }
