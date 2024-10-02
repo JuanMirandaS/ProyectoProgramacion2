1d2eb37aab59c081efb8e420ce4ce7cba19a1df9
josen <josen@JOSE>
2024-10-02 01:56:50 -0300

    Actualizacion UsuarioResponses

--- ProyectoProgramacion2/Response/UsuarioResponses.cs
+++ ProyectoProgramacion2/Response/UsuarioResponses.cs
@@ -2,20 +2,18 @@
 
 namespace ProyectoProgramacion2.Response
 {
-    public class UsuarioResponses : ResponseBase<Usuario>
+    public class UsuarioResponses
     {
+        public class UsuarioResponse : ResponseBase<Usuario>
+        {
+        }
+        public class UsuariosResponse : ResponseBase<List<Usuario>>
+        {
+        }
 
-    }
-    public class UsuariosResponses : ResponseBase<List<Usuario>>
-    {
-
-    }
-    public class UsuarioNuevoResponse : ResponseBase<bool> 
-    {
-
-    }
-    public class UsuarioActualizarResponse : ResponseBase<bool>
-    {
+        public class NuevoUsuarioResponse : ResponseBase<bool>
+        {
+        }
 
     }
 }
