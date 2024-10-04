ca723776824dba4c54296b68445e9dae8f695ddd
AhuEst <fabianestahuca@gmail.com>
2024-10-03 23:26:34 -0300

    Migracion Corregida

---
+++ ProyectoProgramacion2/Migrations/20241004021818_Migracion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoProgramacion2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
